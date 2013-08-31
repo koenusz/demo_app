515fd24446bfd92e092813c424bb24033625d5f7
Koen van Dijk <kjw.vandijk@gmail.com>
2013-08-31 15:58:49 +0200

    finished demo app

---
+++ app/models/micro_post.rb
@@ -0,0 +1,4 @@
+class MicroPost < ActiveRecord::Base
+	belongs_to :user
+	validate :content, length: {maximum: 140}
+end
---
+++ app/views/micro_posts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @micro_post, :content, :user_id, :created_at, :updated_at
